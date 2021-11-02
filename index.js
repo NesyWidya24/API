e6058e0d97854c72081a9b188bb8a54e342bace6
Nesy Widyastuti <nessywidya@gmail.com>
2021-11-02 19:06:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect('mongodb://localhost/ourdata');
+mongoose.Promise = global.Promise;
+
+app.use(express.json());
+
+// app.get('/api', (req, res) => res.send('API Working!!'));
+app.use('/api', require('./routes/api'));
+
+app.use(function (err, req, res, next) {
+    res.status(422).send({error: err.message});
+})
+
+app.listen(process.env.port || 4000, function () {
+    console.log('Now Listening for request');
+})
